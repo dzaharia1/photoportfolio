81c138ba7a5e4cbc17647603d721cfe674790e1e
danzaharia <zaharia.danny@gmail.com>
2015-10-24 11:25:48 -0500

    correctly serve up the index

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 var express = require('express');
-
 var app = express();
 
 app.use(express.static(__dirname + '/', {extensions:['html']}));
 
-var host = (process.env.VCAP_APP_HOST || 'localhost');
 var port = (process.env.VCAP_APP_PORT || '4000');
 
-app.listen(port, host);
-console.log('App is running on port ' + port);
+app.get('/', function (req, res) {
+  res.sendFile('index.html');
+});
+
+app.listen(port, function() {
+  console.log('app is listening on port ' + port);
+});
